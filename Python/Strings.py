312336a110752d69cf44ccb0fc6d1641c48847e7
shrutesh27 <shruteshthombare@gmail.com>
2026-07-28 00:46:08 +0530

    Strings1

---
+++ Python/Strings.py
@@ -0,0 +1,22 @@
+#String are sequence of characters
+#in python strings are sequence of unicode characters
+#String -> ACII value(only for eng lang) but 
+# unicode is combination of multiple lagnuage-> Binary value
+
+#Creating Strings
+c='Hello'
+print(c)
+c="Hello"
+print(c)
+#double used because of below
+print("It's raining outside")#'' can used as apostapy
+
+#also can use triple inverted comma as when we want to 
+# print multi strings -> like blog's
+print("""HI this is shrutesh thombare i am an qa enigneer""")
+
+#Type conversion
+c=str("Hello")
+print(c)
+c=str(123)
+print(c+" hello")
